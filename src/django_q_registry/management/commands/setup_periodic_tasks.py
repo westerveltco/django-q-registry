dc0d092b9fe6d17b1f3f5e676b45b3a84d982efb
Josh <josh@joshthomas.dev>
2023-08-26 13:36:17 -0500

    add noqa comments

--- src/django_q_registry/management/commands/setup_periodic_tasks.py
+++ src/django_q_registry/management/commands/setup_periodic_tasks.py
@@ -1,9 +1,12 @@
+from __future__ import annotations
+
 from django.core.management.base import BaseCommand
+
 from django_q_registry.registry import registry
 
 
 class Command(BaseCommand):
-    help = "Register all tasks in the registry"
+    help = "Register all tasks in the registry"  # noqa: A003
 
-    def handle(self, *args, **options):
+    def handle(self, *args, **options):  # noqa: ARG002
         registry.register_all()
