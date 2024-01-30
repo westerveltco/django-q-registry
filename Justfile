d9c77d0455a832412ca2e43f6a1b7328643dc178
Josh <josh@joshthomas.dev>
2024-01-30 09:05:46 -0600

    update project scaffolding

--- Justfile
+++ Justfile
@@ -1,11 +1,93 @@
 set dotenv-load := true
 
 @_default:
-  just --list
+    just --list
 
-dev:
-  python -m pip install -U pip
-  python -m pip install '.[dev]'
+# ----------------------------------------------------------------------
+# DEPENDENCIES
+# ----------------------------------------------------------------------
 
-test:
-  python -m nox --reuse-existing-virtualenvs
+alias install := bootstrap
+
+bootstrap:
+    python -m pip install --editable '.[dev]'
+
+pup:
+    python -m pip install --upgrade pip
+
+update:
+    @just pup
+    @just bootstrap
+
+# ----------------------------------------------------------------------
+# TESTING/TYPES
+# ----------------------------------------------------------------------
+
+test *ARGS:
+    python -m nox --reuse-existing-virtualenvs --session "test" -- "{{ ARGS }}"
+
+test-all *ARGS:
+    python -m nox --reuse-existing-virtualenvs --session "tests" -- "{{ ARGS }}"
+
+coverage:
+    python -m nox --reuse-existing-virtualenvs --session "coverage"
+
+types:
+    python -m nox --reuse-existing-virtualenvs --session "mypy"
+
+# ----------------------------------------------------------------------
+# DJANGO
+# ----------------------------------------------------------------------
+
+manage *COMMAND:
+    #!/usr/bin/env python
+    import sys
+
+    try:
+        from django.conf import settings
+        from django.core.management import execute_from_command_line
+    except ImportError as exc:
+        raise ImportError(
+            "Couldn't import Django. Are you sure it's installed and "
+            "available on your PYTHONPATH environment variable? Did you "
+            "forget to activate a virtual environment?"
+        ) from exc
+
+    settings.configure(INSTALLED_APPS=["django_q_registry"])
+    execute_from_command_line(sys.argv + "{{ COMMAND }}".split(" "))
+
+alias mm := makemigrations
+
+makemigrations *APPS:
+    @just manage makemigrations {{ APPS }}
+
+migrate *ARGS:
+    @just manage migrate {{ ARGS }}
+
+# ----------------------------------------------------------------------
+# DOCS
+# ----------------------------------------------------------------------
+
+@docs-install:
+    python -m pip install '.[docs]'
+
+@docs-serve:
+    #!/usr/bin/env sh
+    if [ -f "/.dockerenv" ]; then
+        sphinx-autobuild docs docs/_build/html --host "0.0.0.0"
+    else
+        sphinx-autobuild docs docs/_build/html --host "localhost"
+    fi
+
+@docs-build LOCATION="docs/_build/html":
+    sphinx-build docs {{ LOCATION }}
+
+# ----------------------------------------------------------------------
+# UTILS
+# ----------------------------------------------------------------------
+
+lint:
+    python -m nox --reuse-existing-virtualenvs --session "lint"
+
+mypy:
+    python -m nox --reuse-existing-virtualenvs --session "mypy"
--- noxfile.py
+++ noxfile.py
@@ -1,5 +1,8 @@
 from __future__ import annotations
 
+import os
+from pathlib import Path
+
 import nox
 
 PY38 = "3.8"
@@ -8,16 +11,18 @@ PY310 = "3.10"
 PY311 = "3.11"
 PY312 = "3.12"
 PY_VERSIONS = [PY38, PY39, PY310, PY311, PY312]
-PY_DEFAULT = PY38
+PY_DEFAULT = PY_VERSIONS[0]
+PY_LATEST = PY_VERSIONS[-1]
 
 DJ32 = "3.2"
-DJ40 = "4.0"
-DJ41 = "4.1"
 DJ42 = "4.2"
+DJ50 = "5.0"
 DJMAIN = "main"
 DJMAIN_MIN_PY = PY310
-DJ_VERSIONS = [DJ32, DJ40, DJ41, DJ42, DJMAIN]
-DJ_DEFAULT = DJ32
+DJ_VERSIONS = [DJ32, DJ42, DJ50, DJMAIN]
+DJ_LTS = [DJ32, DJ42]
+DJ_DEFAULT = DJ_LTS[-1]
+DJ_LATEST = DJ_VERSIONS[-2]
 
 
 def version(ver: str) -> tuple[int, ...]:
@@ -33,16 +38,28 @@ def should_skip(python: str, django: str) -> tuple[bool, str | None]:
     if django == DJ32 and version(python) >= version(PY312):
         return True, f"Django {DJ32} requires Python < {PY312}"
 
+    if django == DJ50 and version(python) < version(PY310):
+        return True, f"Django {DJ50} requires Python {PY310}+"
+
     return False, None
 
 
-@nox.session(python=PY_VERSIONS)
-@nox.parametrize("django", DJ_VERSIONS)
-def tests(session, django):
-    skip = should_skip(session.python, django)
-    if skip[0]:
-        session.skip(skip[1])
+@nox.session
+def test(session):
+    default_test = f"tests(python='{PY_DEFAULT}', django='{DJ_DEFAULT}')"
+    if session.posargs:
+        session.notify(default_test, posargs=session.posargs)
+    else:
+        session.notify(default_test)
+    session.skip()
 
+
+@nox.session
+@nox.parametrize(
+    "python,django",
+    [(python, django) for python in PY_VERSIONS for django in DJ_VERSIONS if not should_skip(python, django)[0]],
+)
+def tests(session, django):
     session.install(".[dev]")
 
     if django == DJMAIN:
@@ -50,4 +67,46 @@ def tests(session, django):
     else:
         session.install(f"django=={django}")
 
-    session.run("pytest", "-n", "auto", "--dist", "loadfile")
+    if session.posargs:
+        session.run("python", "-m", "pytest", *session.posargs)
+    else:
+        session.run("python", "-m", "pytest")
+
+
+@nox.session
+def coverage(session):
+    session.install(".[dev]")
+    session.run("python", "-m", "pytest", "--cov=django_twc_toolbox")
+
+    try:
+        summary = os.environ["GITHUB_STEP_SUMMARY"]
+        with Path(summary).open("a") as output_buffer:
+            output_buffer.write("")
+            output_buffer.write("### Coverage\n\n")
+            output_buffer.flush()
+            session.run(
+                "python",
+                "-m",
+                "coverage",
+                "report",
+                "--skip-covered",
+                "--skip-empty",
+                "--format=markdown",
+                stdout=output_buffer,
+            )
+    except KeyError:
+        session.run("python", "-m", "coverage", "html", "--skip-covered", "--skip-empty")
+
+    session.run("python", "-m", "coverage", "report")
+
+
+@nox.session
+def lint(session):
+    session.install(".[lint]")
+    session.run("python", "-m", "pre_commit", "run", "--all-files")
+
+
+@nox.session
+def mypy(session):
+    session.install(".[dev]")
+    session.run("python", "-m", "mypy", ".")
