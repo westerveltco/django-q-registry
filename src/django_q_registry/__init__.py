e669fb1d4f3d16c46aaa2b337e2c5e557fb75cd4
Josh <josh@joshthomas.dev>
2024-02-09 22:01:38 -0600

    update existing files

--- src/django_q_registry/__init__.py
+++ src/django_q_registry/__init__.py
@@ -3,8 +3,8 @@ from __future__ import annotations
 from django_q_registry.registry import register_task
 
 __version__ = "0.1.0"
+__template_version__ = "2024.7"
 
 __all__ = [
     "register_task",
 ]
-__template_version__ = "2024.7"
