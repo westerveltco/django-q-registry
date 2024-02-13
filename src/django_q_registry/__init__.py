afaca81b821169564ccf6fc5427122e69c7b25a6
Josh <josh@joshthomas.dev>
2024-02-13 13:57:54 -0600

    bump template to v2024.9

--- src/django_q_registry/__init__.py
+++ src/django_q_registry/__init__.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 from django_q_registry.registry import register_task
 
 __version__ = "0.1.0"
-__template_version__ = "2024.8"
+__template_version__ = "2024.9"
 
 __all__ = [
     "register_task",
