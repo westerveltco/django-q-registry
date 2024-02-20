8d00a8aafe6ccdb2f8fd7ed7a812e75500fa6d88
Josh <josh@joshthomas.dev>
2024-02-20 16:37:57 -0600

    :bookmark: bump version 0.2.0 -> 0.2.1

--- src/django_q_registry/__init__.py
+++ src/django_q_registry/__init__.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 from django_q_registry.registry import register_task
 
-__version__ = "0.2.0"
+__version__ = "0.2.1"
 __template_version__ = "2024.12"
 
 __all__ = [
--- tests/test_version.py
+++ tests/test_version.py
@@ -4,4 +4,4 @@ from django_q_registry import __version__
 
 
 def test_version():
-    assert __version__ == "0.2.0"
+    assert __version__ == "0.2.1"
