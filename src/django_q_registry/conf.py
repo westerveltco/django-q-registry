fab476f40803c48525a3de12676abafdf0d8f300
Josh <josh@joshthomas.dev>
2024-08-01 14:42:04 -0500

    change to Any

--- src/django_q_registry/conf.py
+++ src/django_q_registry/conf.py
@@ -1,6 +1,8 @@
+# pyright: reportAny=false
 from __future__ import annotations
 
 from dataclasses import dataclass
+from typing import Any
 
 from django.conf import settings
 
@@ -12,12 +14,12 @@ DJANGO_Q_REGISTRY_SETTINGS_NAME = "Q_REGISTRY"
 @dataclass(frozen=True)
 class AppSettings:
     PERIODIC_TASK_SUFFIX: str = " - QREGISTRY"
-    TASKS: list[dict[str, object]] = []
+    TASKS: list[dict[str, Any]] = []
 
     @override
     def __getattribute__(self, __name: str) -> object:
         user_settings = getattr(settings, DJANGO_Q_REGISTRY_SETTINGS_NAME, {})
-        return user_settings.get(__name, super().__getattribute__(__name))  # pyright: ignore[reportAny]
+        return user_settings.get(__name, super().__getattribute__(__name))
 
 
 app_settings = AppSettings()
